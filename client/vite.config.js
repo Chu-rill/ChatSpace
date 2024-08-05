a97df04f7bc0bf3a5a5b6d79e61996dc1ea4ad4c
Chu-rill <churchillrimamskep@gmail.com>
2024-08-05 12:08:11 +0100

    changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     proxy: {
       "/api": {
         target: "http://localhost:3001",
