322aa8df5bf4da7537c97631992a3eb68c123712
Chu-rill <churchillrimamskep@gmail.com>
2024-08-09 17:40:39 +0100

    wrong link on socket

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -58,7 +58,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (authUser) {
       const socket = io(
-        "http://127.0.0.1:5001/chatspace-caee5/us-central1/api",
+        "https://us-central1-chatspace-caee5.cloudfunctions.net/api",
         // "http://localhost:3001",
         {
           query: {
