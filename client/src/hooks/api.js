ebc8fea099c74bb9fc855d3940889ed0188fc921
Chu-rill <churchillrimamskep@gmail.com>
2024-08-12 02:03:21 +0100

    removed the forward slash at the end of the url

--- client/src/hooks/api.js
+++ client/src/hooks/api.js
@@ -1,4 +1,4 @@
-export const liveLink = "https://chatspace-backend.onrender.com/";
+export const liveLink = "https://chatspace-backend.onrender.com";
 export const localLink =
   "http://127.0.0.1:5001/chatspace-caee5/us-central1/api";
 export const localLink2 = "http://localhost:3001";
